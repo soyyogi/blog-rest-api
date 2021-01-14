06093be994610ec841c5fed845f5f1c45f8e2f00
fabrizio <faber.rvn@gmail.com>
2021-01-14 12:07:17 +0100

    Resolve Mongodb Connection Issue

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const port = 3000 || process.env.PORT
+
+const port = 3000 || process.env.PORT;
 
 const app = express();
 
 // app.use('engine-views', 'ejs');
 app.use(express.static('public'));
 
-const mongoConnect = mongoose.createConnection('mongodb://localhost:27017/blogApi');
+mongoose.connect('mongodb://localhost:27017/blogApi');
 
 app.listen( port, () => console.log('Server up and running on port 3000'));
