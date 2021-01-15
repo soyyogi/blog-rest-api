c6c416cf0b79dff440aadb60d7409a716092829e
fabrizio <faber.rvn@gmail.com>
2021-01-15 15:57:13 +0100

    #10 Add axios to get post

--- public/script/main.js
+++ public/script/main.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
-
-document.querySelector('#home-link').addEventListener('click', () => {
-  axios.get('/posts', {
+document.querySelector('#home-link').addEventListener('click', async () => {
+  const result = await axios.get('/post', {
     headers: {
-      Authorization: 'Bearer token'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDAxYTdlMGQ5YmQ0ODIzZTExMmJhNjIiLCJpYXQiOjE2MTA3MjEyNDh9.WqH1Pro6uRqlBG5_fFLORS6ZZ7rdFqtbILKjhwsscTU'
     }
   })
+  const posts = result.data;
+  console.log(posts);
 })
