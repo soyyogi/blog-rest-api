1ac4d1494dca0f8ee22dacd0b93f7352f5d2d8b8
yogi <raoyogesh78@yahoo.in>
2021-01-18 01:08:01 +0100

    updated post edit and delete permissions

--- src/Routes/post.js
+++ src/Routes/post.js
@@ -33,14 +33,35 @@ route.post('/post/create', Auth, async (req, res) => {
 })
 
 route.post('/post/delete', Auth, async (req, res) => {
-  await Post.deleteOne({_id: req.body.id});
-  const posts = await  Post.find();
-  res.render('home', {posts})
+  try {
+    const post = await Post.findById(req.body.id);
+    const isValid = post.author.toString() == req.user._id.toString();
+    if (!isValid) {
+      const posts = await  Post.find();
+      return res.status(400).render('error-home', { Error: 'You are not the author', posts });
+    }
+    await post.remove();
+    const posts = await  Post.find();
+    res.render('home', {posts})
+  } catch (error) {
+    const posts = await  Post.find();
+    res.status(500).render('error-home', { Error: error.message, posts });
+  }
 })
 
 route.post('/post/edit', Auth, async (req, res) => {
-  const post = await Post.findById(req.body.id);
-  res.render('edit-post', {post});
+  try {
+    const post = await Post.findById(req.body.id);
+    const isValid = post.author.toString() == req.user._id.toString();
+    if (!isValid) {
+      const posts = await  Post.find();
+      return res.status(400).render('error-home', { Error: 'You are not the author', posts });
+    }
+    res.render('edit-post', {post});
+  } catch (error) {
+    const posts = await  Post.find();
+    res.status(500).render('error-home', { Error: error.message, posts });
+  }
 })
 
 route.post('/post-edit', Auth, async (req, res) => {
@@ -52,76 +73,5 @@ route.post('/post-edit', Auth, async (req, res) => {
   const posts = await  Post.find();
   res.render('home', {posts})
 })
-
-// route.post('/post', async (req, res) => {
-//   const post = new Post(req.body);
-//   post.author = req.user._id;
-//   post.authorName = req.user.name;
-//   post.authorEmail = req.user.email;
-//   try {
-//     await post.save();
-//     res.send(post);
-//   } catch (Error) {
-//     res.status(400).send({ Error: error });
-//   }
-// })
-
-
-// route.get('/post', async (req, res) => {
-//   try {
-//     await req.user.populate('posts').execPopulate()
-//     res.render('home', {posts: req.user.posts})
-//     // res.send(req.user.posts)
-//   } catch (error) {
-//     res.status(500).send(error)
-//   }
-// })
-
-// route.post('/post/delete/:id', async function (req, res) {
-//   try {
-//     const post = await Post.findById(req.params.id);
-//     console.log(req.params.id);
-//     // const isValid = post.author.toString() == req.user._id.toString();
-//     // if (!isValid) {
-//     //   return res.status(400).send({ Error: 'You are not the author' })
-//     // }
-//     await post.remove();
-//     res.redirect('/posts');
-//   } catch (error) {
-//     res.status(500).send();
-//   }
-// })
-
-// route.patch('/post/:id', async (req, res) => {
-//   const _id = req.params.id
-//   const updates = Object.keys(req.body)
-//   const allowedUpdates = ['title', 'body']
-
-//   const isValidUpdate = updates.every(update => allowedUpdates.includes(update))
-
-//   if (!isValidUpdate) {
-//     return res.status(400).send({ error: 'Invalid Updates' })
-//   }
-
-//   try {
-//     const post = await Post.findById(req.params.id);
-//     const isValid = post.author.toString() == req.user._id.toString();
-//     if (!isValid) {
-//       return res.status(400).send({ Error: 'You are not the author' })
-//     }
-//     updates.forEach(update => post[update] = req.body[update])
-//     await post.save()
-
-//     if (!post) {
-//       return res.status(404).send()
-//     }
-
-//     res.send(post)
-//   } catch (error) {
-//     res.status(400).send(error)
-//   }
-// })
-
-
 
 module.exports = route;
